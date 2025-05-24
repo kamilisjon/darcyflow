443ab0cdad7c311f3c3e28aadae0979dded2d0f0
KamilisJ <kamjon@ktu.lt>
2025-05-24 09:16:20 +0300

    permiability more explicit

--- examples/run_tpfa.py
+++ examples/run_tpfa.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import uniform_filter
 
 
-def TPFA(grid:dict[str, int], cell_size:dict[str, float], K:NDArray, pressure_bc:dict[int, int]) -> NDArray:
+def TPFA(grid:dict[str, int], cell_size:dict[str, float], permiability_filed:NDArray, pressure_bc:dict[int, int]) -> NDArray:
     # Inverse permeabilities
-    L = 1.0 / K
+    perm_inv = 1.0 / permiability_filed
 
     # Transmissibilities
     TX = np.zeros((grid['Nx']+1, grid['Ny']))
@@ -17,10 +17,10 @@ def TPFA(grid:dict[str, int], cell_size:dict[str, float], K:NDArray, pressure_bc
 
     for i in range(1, grid['Nx']):
         for j in range(grid['Ny']):
-            TX[i,j] = tx / (L[0,i-1,j] + L[0,i,j])
+            TX[i,j] = tx / (perm_inv[0,i-1,j] + perm_inv[0,i,j])
     for i in range(grid['Nx']):
         for j in range(1, grid['Ny']):
-            TY[i,j] = ty / (L[1,i,j-1] + L[1,i,j])
+            TY[i,j] = ty / (perm_inv[1,i,j-1] + perm_inv[1,i,j])
 
     # Assemble A
     def idx(i:int, j:int) -> int: return i + j*grid['Nx']
@@ -39,10 +39,11 @@ def TPFA(grid:dict[str, int], cell_size:dict[str, float], K:NDArray, pressure_bc
                 t = TY[i,j+1]; rows.append(m); cols.append(idx(i,j+1)); data.append(-t); diag+=t
             rows.append(m); cols.append(m); data.append(diag)
 
-    # impose Dirichlet BCs
     area = grid['Nx'] * grid['Ny']
     A = lil_matrix((area, area))
     A[rows, cols] = data
+
+    # impose Dirichlet BCs
     q = np.zeros(area)
     for node, pval in pressure_bc.items():
         A[node,:] = 0
@@ -59,16 +60,14 @@ if __name__=='__main__':
     cell_sizes: dict[str, float] = {'hx':1/40, 'hy':1/40}
 
     if homogeneous:
-        K = np.ones((2,grid['Nx'],grid['Ny']))
+        perm = np.ones((2,grid['Nx'],grid['Ny']))
     else:
-        rnd = np.random.randn(2,grid['Nx'],grid['Ny'])
-        S = uniform_filter(rnd, size=3, mode='reflect')
-        K = np.exp(5*uniform_filter(S, size=3, mode='reflect'))
+        perm = np.exp(5*uniform_filter(uniform_filter(np.random.randn(2,grid['Nx'],grid['Ny']), size=3, mode='reflect'), size=3, mode='reflect'))
 
-    pressure_tpfa = TPFA(grid, cell_sizes, K, {0: +300, grid['Nx']*grid['Ny']-1: -300})
+    pressure_tpfa = TPFA(grid, cell_sizes, perm, {0: +300, grid['Nx']*grid['Ny']-1: -300})
 
     fig, (ax1,ax2) = plt.subplots(1,2,figsize=(8,4))
-    im = ax1.imshow(np.log10(K[0]), origin='lower', aspect='equal')
+    im = ax1.imshow(np.log10(perm[0]), origin='lower', aspect='equal')
     ax1.set_title('log10(K)'); fig.colorbar(im, ax=ax1)
 
     cf = ax2.contourf(pressure_tpfa, levels=20)
